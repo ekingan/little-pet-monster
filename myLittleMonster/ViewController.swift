2bae5a6cb559d8e618cf134665b664129492c618
ekingan <ekingan@gmail.com>
2016-06-16 17:34:39 -0700

    little monster done

--- myLittleMonster/ViewController.swift
+++ myLittleMonster/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 
 class ViewController: UIViewController {
@@ -35,6 +36,12 @@ class ViewController: UIViewController {
     
     var currentItem: UInt32 = 0
     
+    var musicPlayer: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxBite: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,6 +55,31 @@ class ViewController: UIViewController {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
         
         startTimer()
+        
+        do {
+            let resourcePath = NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")!
+            let url = NSURL(fileURLWithPath: resourcePath)
+            try musicPlayer = AVAudioPlayer(contentsOfURL: url)
+            
+            try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+            
+            try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+            
+            try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+            
+            try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+            
+            musicPlayer.prepareToPlay()
+            musicPlayer.play()
+            
+            sfxBite.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            sfxSkull.prepareToPlay()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func itemDroppedOnCharacter(notif: AnyObject) {
@@ -59,6 +91,12 @@ class ViewController: UIViewController {
         
         heartImg.alpha = DIM_ALPHA
         heartImg.userInteractionEnabled = false
+        
+        if currentItem == 0 {
+            sfxHeart.play()
+        } else {
+            sfxBite.play()
+        }
         
         
         
@@ -77,6 +115,9 @@ class ViewController: UIViewController {
         
         if !monsterHappy {
             penalties++
+            
+            sfxSkull.play()
+            
             if penalties == 1 {
                 penalty1Img.alpha = OPAQUE
                 penalty2Img.alpha = DIM_ALPHA
@@ -122,6 +163,8 @@ class ViewController: UIViewController {
     func gameOver() {
         timer.invalidate()
         monsterImg.playDeathAnimations()
+        
+        sfxDeath.play()
     }
 
 }
